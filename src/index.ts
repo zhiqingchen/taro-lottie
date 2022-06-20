8cefcabd71300de0c7d6da2898b1afe5a231d133
chenzhiqing01 <329604487@qq.com>
2022-06-20 15:52:29 +0800

    feat: implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export * from './lib/number';
+import type { AnimatedLottieViewProps } from 'lottie-react-native'
+import { ComponentType } from 'react'
+
+let implementation
+if (process.env.TARO_ENV === 'weapp') {
+  implementation = require('./lib/mp')
+} else if (process.env.TARO_ENV === 'h5') {
+  implementation = require('./lib/web')
+} else {
+  implementation = require('./lib/rn')
+}
+const lottie: ComponentType<AnimatedLottieViewProps> = implementation.default || implementation
+export default lottie
---
+++ src/types/taro-lottie.d.ts
@@ -0,0 +1,2 @@
+declare module 'taro-lottie' {
+}
